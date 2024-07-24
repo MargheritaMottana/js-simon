3666057396fdc57b5385a87f9a4da78fc8586c16
MargheritaMottana <margherita.mottana@gmail.com>
2024-07-24 18:23:25 +0200

    scrittura html e css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('Hello :3')
+
+/*
+Descrizione:
+1. Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+2. Dopo 30 secondi i numeri scompaiono e 
+3. l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite i prompt().
+4. Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/

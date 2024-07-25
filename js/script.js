3c24154b6b885b3f64de1b652d345446d1216392
MargheritaMottana <margherita.mottana@gmail.com>
2024-07-25 20:31:46 +0200

    aggiunto messaggio per dire all'utente quanti e quali numeri ha indovinato + aggiunta timer in pagina + piccoli aggiustamenti css

--- js/script.js
+++ js/script.js
@@ -16,6 +16,12 @@ const random = []
 
 const numberContainer = document.getElementById('numberContainer')
 
+const timer = document.getElementById('timer')
+
+const risultato = document.getElementById('result')
+
+let numeroUtente = 0
+
 // devo creare 5 elementi in pagina
 // creo un ciclo che mi scriva 5 elementi
 
@@ -32,10 +38,14 @@ for (let i = 1; i <= 5; i++) {
 };
 
 // devo creare un timer che parte all'avvio della pagina e che duri 30 secondi
-let counter = 3;
+let counter = 30;
 
 let clock = setInterval(function () {
     console.log(counter);
+
+    // aggiungo il timer in pagina
+    timer.innerHTML = `<div class="timer"> ${counter}
+        </div>`;
 
     // se il timer arriva a 0 fermati
     if (counter == 0) {
@@ -43,14 +53,38 @@ let clock = setInterval(function () {
         clearInterval(clock);
         clock = null;
 
+        //al termine del timer, svuoto il timer
+        timer.innerHTML = '';
+
         // al termine del timer, svuoto il contenuto dell'html e ci metto del nuovo testo
         numberContainer.innerHTML = ` Ora inserisci nei prompt i numeri che hai visto, rispettandone l'ordine. `;
+
+        // aggiungo una variabile per il numero indovinato
+        let numeroIndovinato = 0
+        // aggiungo variabile anche per la lista di numeri indovinati
+        let listaIndovinati = []
 
         setTimeout(function () {
             // alla fine del timer inserisco i prompt
             for (let i = 0; i < 5; i++) {
-                const numeroUtente = parseInt(prompt(`Inserisci qui il ${i + 1}° numero`))
-            };
+                let numeroUtente = parseInt(prompt(`Inserisci qui il ${i + 1}° numero`))
+
+                // aggiungere paragone tra array e numero dell'utente per verificare se, quali e quanti ne ha indovinati
+                if (numeroUtente == random[i]) {
+                    numeroIndovinato++;
+
+                    //pusho il numero indovinato nell'array per creare la lista
+                    listaIndovinati.push(numeroUtente);
+
+                    // svuoto html
+                    numberContainer.innerHTML = ''
+                    // inserisco nel risultato il messaggio
+                    // questa scrittura serve per mettere una virgola tra gli elementi dell'array --> listaIndovinati.join(' ,')
+                    // il join prende tutti gli elementi dell'array, li mette in una stringa e li separa con quello che metto tra le parentesi.
+                    risultato.innerHTML = `Complimenti! <br> Hai indovinato ${numeroIndovinato} numeri, <br> in particolare questi: ${listaIndovinati.join(' ,')}.`;
+                }
+            }
+
         }, 0.5 * 1000)
 
     }
